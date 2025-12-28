c2d59950272a2f937ede4175c48998ad44b70bc3
Brandi Seeley <brandi.n.seeley@gmail.com>
2025-12-28 10:52:28 -0700

    Create test button to bypass manual testing steps

--- popup.js
+++ popup.js
@@ -8,4 +8,10 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log("API Key stored as:", apiKey);
     });
   });
+
+  const testButton = document.getElementById("testButton");
+  testButton.addEventListener("click", async() => {
+    console.log("Test button clicked");
+    console.log(await parseEvent("LS Women's Group | Fundamentals at Work | Sunday, December 21st | 12PM ET / 9AM PT / 6PM CET"));
+  });
 });
