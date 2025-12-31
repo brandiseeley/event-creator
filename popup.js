486ae1c5240db6c983b869547d205dd3d7aba9b9
Brandi Seeley <brandi.n.seeley@gmail.com>
2025-12-31 11:57:57 -0700

    Add initial loading/success/error/idle states

--- popup.js
+++ popup.js
@@ -20,3 +20,75 @@ testButton.addEventListener('click', async () => {
     console.error(err);
   }
 });
+
+const status = document.getElementById('status');
+const calendarUrl = document.getElementById('calendarUrl');
+const error = document.getElementById('error');
+
+function hideAll() {
+  status.textContent = '';
+  calendarUrl.textContent = '';
+  calendarUrl.href = '#';
+  error.textContent = '';
+}
+
+function showLoading() {
+  status.textContent = 'Loading. Please wait...';
+}
+
+function showSuccess(Url) {
+  status.textContent = 'Success!';
+  calendarUrl.textContent = 'Google Calendar URL';
+  calendarUrl.href = Url;
+
+  calendarUrl.addEventListener('click', (event) => {
+    event.preventDefault();
+    chrome.tabs.create({ url: Url });
+  });
+}
+
+function showError(message) {
+  error.textContent = message;
+}
+
+function showIdle() {
+  hideAll();
+}
+
+// Status may be: loading, success, error, idle
+function render({ status, calendarUrl, error }) {
+  hideAll();
+
+  switch (status) {
+    case 'loading':
+      showLoading();
+      break;
+    case 'success':
+      showSuccess(calendarUrl);
+      break;
+    case 'error':
+      showError(error);
+      break;
+    default:
+      showIdle();
+  }
+}
+
+chrome.storage.onChanged.addListener((changes, area) => {
+  if (area !== 'local') return;
+
+  if (changes.status) {
+    chrome.storage.local.get([
+      'status',
+      'calendarUrl',
+      'error',
+    ]).then(render);
+  }
+});
+
+// Initial render
+chrome.storage.local.get([
+  'status',
+  'calendarUrl',
+  'error',
+]).then(render);
--- service_worker.js
+++ service_worker.js
@@ -14,10 +14,23 @@ chrome.contextMenus.onClicked.addListener(async (info) => {
   if (info.menuItemId !== 'create-event') return;
 
   try {
+    // Set status to loading and save text
+    chrome.storage.local.set({
+      status: 'loading',
+      inputText: info.selectionText,
+    });
+
+    // Open popup to show loading status
+    chrome.action.openPopup();
+
+    // Generate event
     const url = await createCalendarLinkFromText(info.selectionText);
 
-    // Open the calendar immediately
-    chrome.tabs.create({ url });
+    // Set status to success
+    chrome.storage.local.set({
+      status: 'success',
+      calendarUrl: url,
+    });
   } catch (err) {
     console.error(err);
   }
